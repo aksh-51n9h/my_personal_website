50424abd2a8a5cddee6ac515d987be94b6762c74
aksh-51n9h <singh2104.akshay@gmail.com>
2022-02-07 20:32:27 +0530

    scripts.js - add contact form submit handler

--- public/script/scripts.js
+++ public/script/scripts.js
@@ -1,17 +1,11 @@
 let submitButton = document.getElementById("submit");
 
-submitButton.disabled = true;
-
 let inputName = document.getElementById("inputName");
 let inputEmail = document.getElementById("inputEmail");
 
 let messageTextarea = document.getElementById("messageTextarea");
 
-messageTextarea.addEventListener("keypress", function (event) { 
-
-    if (messageTextarea.value.length > 0) {
-        submitButton.disabled = false;
-    } else {
-        submitButton.disabled = true;
-    }
+$("#contact-form").submit(function (event) {
+    event.preventDefault();
+    alert("Message sent successfully!");
 });
