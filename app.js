a1a29bbe7658a924795d24989a29e394e2ad93a7
aksh-51n9h <singh2104.akshay@gmail.com>
2022-02-07 15:24:18 +0530

    add post method for contact form

--- app.js
+++ app.js
@@ -10,9 +10,20 @@ const port = process.env.PORT || 3000;
 
 const navItems = ["About", "Work", "Contact"];
 
-app.get("/", function (_, res) { 
-    res.render("index", {navItems: navItems});
+app.get("/", function (_, res) {
+    res.render("index", { navItems: navItems, activeFlag: "active" });
 });
+
+app.route("/contact")
+    .post(function (req, res) {
+        const name = req.body.sender_name;
+        const email = req.body.sender_email;
+        const message = req.body.message;
+
+        console.log(name + "," + email + "," + message);
+
+        res.send("");
+    });
 
 app.listen(port, function () {
     console.log("Server started at port no. " + port);
